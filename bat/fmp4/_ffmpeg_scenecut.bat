4c72e8402ea9a6ac15ad2c066784f2ca144ecbf2
unknown <edwinbradford@gmail.com>
2018-11-05 20:58:58 +0000

    Fixes to _ffmpeg_scenecut.bat

--- bat/fmp4/_ffmpeg_scenecut.bat
+++ bat/fmp4/_ffmpeg_scenecut.bat
@@ -69,8 +69,6 @@ rmdir "mp4" /s /q
 
 :MakeMp4
 
-REM Include temp directory for first of two pass encode
-if not exist "temp" mkdir "temp"
 mkdir "mp4"
 
 :Resolution
@@ -79,13 +77,13 @@ echo Please select a video resolution:
 echo.
 echo 1. Press [1] for 1080p && echo.
 echo 2. Press [2] for 720p && echo.
-echo 3. Press [3] for 480p && echo.
+echo 3. Press [3] for 540p && echo.
 echo 4. Press [4] for 360p && echo.
 
 set /p op=Type option:
 if "%op%"=="1" goto m1080p
 if "%op%"=="2" goto m720p
-if "%op%"=="3" goto m480p
+if "%op%"=="3" goto m540p
 if "%op%"=="4" goto m360p
 
 echo Please select an option:
@@ -93,7 +91,7 @@ goto Resoluion
 
 :m1080p
 
-echo. && echo FFmpeg will now encode the source file at 1080p for streaming, press any key to continue... && echo.
+echo. && echo FFmpeg will now encode the source file at resolutions up to 1080p for streaming, press any key to continue... && echo.
 
 pause > nul
 
@@ -101,23 +99,23 @@ goto 1080p
 
 :m720p
 
-echo. && echo FFmpeg will now encode the source file at 720p for streaming, press any key to continue... && echo.
+echo. && echo FFmpeg will now encode the source file at resolutions up to 720p for streaming, press any key to continue... && echo.
 
 pause > nul
 
 goto 720p
 
-:m480p
+:m540p
 
-echo. && echo FFmpeg will now encode the source file at 480p for streaming, press any key to continue... && echo.
+echo. && echo FFmpeg will now encode the source file at resolutions up to 540p for streaming, press any key to continue... && echo.
 
 pause > nul
 
-goto 480p
+goto 540p
 
 :m360p
 
-echo. && echo FFmpeg will now encode the source file at 360p for streaming, press any key to continue... && echo.
+echo. && echo FFmpeg will now encode the source file at resolutions up to 360p for streaming, press any key to continue... && echo.
 
 pause > nul
 
@@ -128,319 +126,110 @@ REM MPEG DASH encoding recommendations are CBR or VBR with VBR Max 110% and Buff
 
 :1080p
 
-echo Encoding 1080p 7500k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 7500k -maxrate 7500k -bufsize 7500k ^
--preset slow ^
--vf "scale=-2:1080" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/1080p_07500k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
+echo Encoding 1080p 7800k
 ffmpeg -i %input% -an ^
 -movflags +faststart ^
 -metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 7500k -maxrate 7500k -bufsize 7500k ^
+-c:v libx264 -profile:v high -level:v 4.1 ^
 -preset slow ^
 -vf "scale=-2:1080" ^
+-r %framerate% ^
+-b:v 7800k -minrate 7800k -maxrate 8500k -bufsize 17160k ^
 -force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
 -x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/1080p_07500k.log ^
--pass 2 ^
--y mp4/1080p_07500k.mp4
+-vstats_file logs/1080p_7800k.log ^
+-y mp4/07800k_1080p_h264.mp4
 echo.
 
-echo Encoding 1080p 6000k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 6000k -maxrate 6600k -bufsize 13200k ^
--preset slow ^
--vf "scale=-2:1080" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/1080p_06000k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
+echo Encoding 1080p 6000k
 ffmpeg -i %input% -an ^
 -movflags +faststart ^
 -metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 6000k -maxrate 6600k -bufsize 13200k ^
+-c:v libx264 -profile:v high -level:v 4.1 ^
 -preset slow ^
 -vf "scale=-2:1080" ^
+-r %framerate% ^
+-b:v 6000k -minrate 6000k -maxrate 6600k -bufsize 13200k ^
 -force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
 -x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/1080p_06000k.log ^
--pass 2 ^
--y mp4/1080p_06000k.mp4
+-vstats_file logs/1080p_6000k.log ^
+-y mp4/06000k_1080p_h264.mp4
 echo.
 
-echo Encoding 1080p 4500k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 4500k -maxrate 4500k -bufsize 4500k ^
--preset slow ^
--vf "scale=-2:1080" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/1080p_04500k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
+echo Encoding 1080p 4500k
 ffmpeg -i %input% -an ^
 -movflags +faststart ^
 -metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 4500k -maxrate 4500k -bufsize 4500k ^
+-c:v libx264 -profile:v high -level:v 4.1 ^
 -preset slow ^
 -vf "scale=-2:1080" ^
+-r %framerate% ^
+-b:v 4500k -minrate 4500k -maxrate 4950k -bufsize 9900k ^
 -force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
 -x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/1080p_04500k.log ^
--pass 2 ^
--y mp4/1080p_04500k.mp4
+-vstats_file logs/1080p_4500k.log ^
+-y mp4/04500k_1080p_h264.mp4
 echo.
-
-echo Encoding 1080p 3000k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 3000k -maxrate 3000k -bufsize 3000k ^
--preset slow ^
--vf "scale=-2:1080" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/1080p_03000k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
-ffmpeg -i %input% -an ^
--movflags +faststart ^
--metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 3000k -maxrate 3000k -bufsize 3000k ^
--preset slow ^
--vf "scale=-2:1080" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/1080p_03000k.log ^
--pass 2 ^
--y mp4/1080p_03000k.mp4
-echo.
-
-goto Audio
 
 :720p
 
-echo Encoding 720p 4000k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 4000k -maxrate 4000k -bufsize 4000k ^
--preset slow ^
--vf "scale=-2:720" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/720p_04000k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
+echo Encoding 720p 3000k
 ffmpeg -i %input% -an ^
 -movflags +faststart ^
 -metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 4000k -maxrate 4000k -bufsize 4000k ^
+-c:v libx264 -profile:v high -level:v 3.1 ^
 -preset slow ^
 -vf "scale=-2:720" ^
+-r %framerate% ^
+-b:v 3000k -minrate 3000k -maxrate 3300k -bufsize 6600k ^
 -force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
 -x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/720p_04000k.log ^
--pass 2 ^
--y mp4/720p_04000k.mp4
+-vstats_file logs/720p_3000k.log ^
+-y mp4/03000k_720p_h264.mp4
 echo.
 
-echo Encoding 720p 3000k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 3000k -maxrate 3000k -bufsize 3000k ^
--preset slow ^
--vf "scale=-2:720" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/720p_03000k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
+:540p
 
+echo Encoding 540p 2000k
 ffmpeg -i %input% -an ^
 -movflags +faststart ^
 -metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 3000k -maxrate 3000k -bufsize 3000k ^
+-c:v libx264 -profile:v high -level:v 3.1 ^
 -preset slow ^
--vf "scale=-2:720" ^
+-vf "scale=-2:540" ^
+-r %framerate% ^
+-b:v 2000k -minrate 2000k -maxrate 2200k -bufsize 4400k ^
 -force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
 -x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/720p_03000k.log ^
--pass 2 ^
--y mp4/720p_03000k.mp4
+-vstats_file logs/540p_2000k.log ^
+-y mp4/02000k_540p_h264.mp4
 echo.
-
-echo Encoding 720p 2000k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 2000k -maxrate 2000k -bufsize 2000k ^
--preset slow ^
--vf "scale=-2:720" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/720p_02000k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
-ffmpeg -i %input% -an ^
--movflags +faststart ^
--metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 2000k -maxrate 2000k -bufsize 2000k ^
--preset slow ^
--vf "scale=-2:720" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/720p_02000k.log ^
--pass 2 ^
--y mp4/720p_02000k.mp4
-echo.
-
-goto Audio
-
-:480p
-
-echo Encoding 480p 2000k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 2000k -maxrate 2000k -bufsize 2000k ^
--preset slow ^
--vf "scale=-2:480" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/480p_02000k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
-ffmpeg -i %input% -an ^
--movflags +faststart ^
--metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 2000k -maxrate 2000k -bufsize 2000k ^
--preset slow ^
--vf "scale=-2:480" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/480p_02000k.log ^
--pass 2 ^
--y mp4/480p_02000k.mp4
-echo.
-
-echo Encoding 480p 1000k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 1000k -maxrate 1000k -bufsize 1000k ^
--preset slow ^
--vf "scale=-2:480" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/480p_01000k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
-ffmpeg -i %input% -an ^
--movflags +faststart ^
--metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 1000k -maxrate 1000k -bufsize 1000k ^
--preset slow ^
--vf "scale=-2:480" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/480p_01000k.log ^
--pass 2 ^
--y mp4/480p_01000k.mp4
-echo.
-
-goto Audio
 
 :360p
 
-echo Encoding 360p 1000k && echo.
-ffmpeg -i %input% -an ^
--r %framerate% ^
--c:v libx264 ^
--b:v 1000k -maxrate 1000k -bufsize 1000k ^
--preset slow ^
--vf "scale=-2:360" ^
--force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
--x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/360p_01000k.log ^
--pass 1 ^
--f mp4 ^
--y temp/null
-
+echo Encoding 360p 1000k
 ffmpeg -i %input% -an ^
 -movflags +faststart ^
 -metadata:s:v:0 language=eng ^
--r %framerate% ^
--c:v libx264 ^
--b:v 1000k -maxrate 1000k -bufsize 1000k ^
+-c:v libx264 -profile:v high -level:v 3.0 ^
 -preset slow ^
 -vf "scale=-2:360" ^
+-r %framerate% ^
+-b:v 1000k -minrate 1000k -maxrate 1100k -bufsize 2200k ^
 -force_key_frames "expr:eq(mod(n,%GOP%),0)" ^
 -x264-params rc-lookahead=%GOP%:keyint=%DoubleGOP%:min-keyint=%GOP% ^
--vstats_file logs/360p_01000k.log ^
--pass 2 ^
--y mp4/360p_01000k.mp4
+-vstats_file logs/360p_1000k.log ^
+-y mp4/01000k_360p_h264.mp4
 echo.
 
 :Audio
 
-echo Encoding aac_low 160k audio && echo.
+echo Encoding aac_low 160k audio
 ffmpeg -i %input% -vn ^
 -metadata:s:a:0 language=eng ^
 -profile:a aac_low -ac 2 -b:a 160k ^
 -y mp4/00160k_aac_lc.mp4
 echo.
-
-REM Delete the first pass folder and contents
-if exist "temp" rmdir "temp" /s /q
 
 REM Change the fragment variable from local to global so the next batch file can access it
 endlocal & (
